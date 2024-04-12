c7f38922cbf130fcd82f5882637c8e2a832b4079
Mateus <mateusoliiveira000@gmail.com>
2024-04-12 15:37:31 -0300

    dd

--- index.php
+++ index.php
@@ -1,33 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Css/index.css">
     <title>Document</title>
 </head>
+
 <body>
 
-<header>
-    
-<h1 class="titulo">  Sistema De Gestão ERP+controle de empresas e de pessoas     </h1>
+    <header>
+        <section>
+            <h1 class="titulo"> Sistema De Gestão ERP+controle de empresas e de pessoas </h1>
 
-<!---Menu----->
-<div class="dois">
-<a class="conhecer"href="conhecer.php">Quero Conhecer</a>
-<a class="entra"href="login.php">Entrar</a>
-</div>
-<!---Fim---->
+            <!---Menu----->
+            <div class="dois">
+                <a class="conhecer" href="conhecer.php">Quero Conhecer</a>
+                <a class="entra" href="login.php">Entrar</a>
+            </div>
+        </section>
+        <!---Fim---->
+    </header>
+    <main>
+        <!-----------------LOGO----------------->
 
- <!-----------------LOGO----------------->
-    
- <img class="logo" src="Img/bitrix-removebg-preview.png" width="300px">
-</header>
+        <img class="logo" src="Img/bitrix-removebg-preview.png" width="300px">
 
-<main>
-<img class="banner1"src=" .Img/bannerr1.jpeg">
 
-</main>
 
+        <img class="banner1" src="Img/bannerr1.jpeg">
+
+        <img class="banner2" src="Img/banner2.jpeg">
+
+    </main>
+
+    <footer>
+
+
+    </footer>
 </body>
+
 </html>
