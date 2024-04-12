641469f940ec30970bd7d8a7c3d69cb15464b95b
Mateus <mateusoliiveira000@gmail.com>
2024-04-12 13:42:43 -0300

    d

--- index.php
+++ index.php
@@ -3,13 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/index.css">
-    <title>Pagina inicial</title>
+    <link rel="stylesheet" href="Css/index.css">
+    <title>Document</title>
 </head>
 <body>
 
-
-
+<header>
     
+<h1 class="titulo">  Sistema De Gestão ERP+controle de empresas e de pessoas     </h1>
+
+<!---Menu----->
+<div class="dois">
+<a class="conhecer"href="conhecer.php">Quero Conhecer</a>
+<a class="entra"href="login.php">Entrar</a>
+</div>
+<!---Fim---->
+
+ <!-----------------LOGO----------------->
+    
+ <img class="logo" src="Img/bitrix-removebg-preview.png" width="300px">
+</header>
+
+<main>
+
+
+</main>
+
 </body>
 </html>
