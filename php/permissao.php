8ff95313bc2f53fa170eca9b1f35e8a6b6466325
Mateus <mateusoliiveira000@gmail.com>
2024-04-17 08:26:14 -0300

    s

---
+++ php/permissao.php
@@ -0,0 +1,32 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Css/permissao.css">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="comeco">
+        
+        <h1 class="titulo"> Sistema De Gestão ERP+controle de empresas e de pessoas </h1>
+
+        <!---Menu----->
+        <div class="dois">
+            <a class="conhecer" href="conhecer.php">Quero Conhecer</a>
+            <a class="entra" href="login.php">Entrar</a>
+        </div>
+        </div>
+    <!---Fim---->
+
+<main>
+    <!-----------------LOGO----------------->
+
+    <img class="logo" src="../Img/bitrix-removebg-preview.png" width="300px">
+
+    
+</body>
+</html>
