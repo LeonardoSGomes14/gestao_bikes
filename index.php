cfc80816ce23ec1d2397e63ab4be82362f47fa1d
kauasilval <kaua60033@gmail.com>
2024-04-17 09:44:53 -0300

    as

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Css/index.css">
-    <title>Document</title>
+    <title>GESTÃO DE BIKES</title>
 </head>
 
 <body>
+
 
     <div class="comeco">
         
