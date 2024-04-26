51a70bd24747d265e32320fe6a40366288de4cc9
KarinaMauro <karinadasilvamauro@gmail.com>
2024-04-26 14:04:38 -0300

    responsivo do sobre - karina

--- GB/php/sobre.php
+++ GB/php/sobre.php
@@ -7,6 +7,139 @@
     <link rel="stylesheet" href="../Css/sobre.css">
     <title>GESTÃO DE BIKES</title>
 </head>
+<style>
+body{
+    background-color:#1f9ea8;
+}
+
+.comeco {
+    background-color: #001e27;
+    width: 100%; 
+    height: 180px;
+    margin-left: -8px;
+    margin-top: -9px;
+    position: absolute;
+}
+.titulo{
+    color: white;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 15px;
+    position: absolute;
+    left: 370px;
+    top: 30px;
+}
+.logo{
+    position: absolute;
+    top: -22%;
+    width: 200px;
+    top: -10px;
+}
+.text1{
+    position: absolute;
+    font-size:38px;
+    margin-top: 185px;
+    left: 220px;
+    color: antiquewhite;
+  }
+
+  .conteudo p {
+    position: absolute;
+    top: 220px;
+    font-size: 21px;
+    font-family: Arial, Helvetica, sans-serif;
+    left: 15px;
+  }
+
+  .conteudo2 p {
+    position: absolute;
+    top: 450px;
+    font-size: 21px;
+    font-family: Arial, Helvetica, sans-serif;
+    left: 655px;
+  }
+
+.bike {
+    position: absolute;
+    top: 420px;
+    left: 40px;
+    width: 580px;
+    height: 250px;
+}
+
+.bike2 {
+    position: absolute;
+    top: 200px;
+    left: 935px;
+    width: 530px;
+    height: 230px;
+}
+
+.rodape{
+
+    margin-top: 680px;
+    background-color: #001e27; 
+    padding: 40px 0; 
+    text-align: center; 
+    }
+
+
+  .entra {
+        text-decoration: none;
+        background-color: transparent;
+        color: rgb(255, 255, 255);
+        border: 1px solid rgb(255, 255, 255);
+        border-radius: 30px;
+        padding: 15px 40px;
+        font-size: 15px;
+        display: inline-block;
+        margin: 11px 400px;
+        transition: background-color 0.3s, color 0.3s, transform 0.3s; /* Adicionando uma transição suave para o tamanho */
+        font-family: Arial, Helvetica, sans-serif;
+      }
+      
+      .entra:hover {
+        transform: scale(1.1); /* Aumentando o tamanho em 10% ao passar o mouse */
+      }
+       /* Adicione esta parte ao seu CSS existente */
+@media screen and (max-width: 768px) {
+    /* Oculta o texto do banner em telas menores que 768px */
+    .titulo {
+        display: none;
+    }
+    .logo {
+        margin-left:35%;
+    }
+    .rodape  {
+        margin-top:1380px;
+        margin-left:-30px;
+    }
+   .entra {
+        margin-left:270px;
+   }  
+   .bike {
+    margin-top:150px;
+    margin-left:-8px;
+    width:540px;
+   }
+   .text1 {
+    margin-top:35%;
+    margin-left:-20px;
+   }
+   .conteudo p {
+    margin-top:70px;
+    margin-left:-20px;
+   }
+   .conteudo2 p {
+    margin-left:-669px;
+    margin-top:410px;
+   }
+   .bike2 {
+    margin-left:-898px;
+    margin-top:900px;
+   }
+
+} 
+</style> 
 
 <body>
 
