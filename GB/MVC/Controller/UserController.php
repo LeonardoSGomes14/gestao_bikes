3a598788cd15eaf2f0848c1f9a1fece1535d255f
Leonardo <leoleonardo131099@gmail.com>
2024-04-26 14:10:10 -0300

    Commit: MVC dos usuários concluído.

--- GB/MVC/Controller/UserController.php
+++ GB/MVC/Controller/UserController.php
@@ -10,9 +10,9 @@ class userController
         $this->usermodel = new userModel($pdo);
     }
 
-    public function criarUser($nome_completo, $datadenascimento, $cpf, $genero, $phone, $email, $senha, $tipo_funcionario, $cep, $cidade, $rua, $numero, $complemento, $hora_entrada, $hora_saida, $carga_horaria, $remuneracao, $data_contratacao, $foto_perfil)
+    public function criarUser($nome_completo, $nome_usuario, $datadenascimento, $cpf, $genero, $phone, $email, $senha, $tipo_funcionario, $cep, $cidade, $rua, $numero, $complemento, $hora_entrada, $hora_saida, $carga_horaria, $remuneracao, $data_contratacao, $foto_perfil)
     {
-        $this->usermodel->criarUser($nome_completo, $datadenascimento, $cpf, $genero, $phone, $email, $senha, $tipo_funcionario, $cep, $cidade, $rua, $numero, $complemento, $hora_entrada, $hora_saida, $carga_horaria, $remuneracao, $data_contratacao, $foto_perfil);
+        $this->usermodel->criarUser($nome_completo, $nome_usuario, $datadenascimento, $cpf, $genero, $phone, $email, $senha, $tipo_funcionario, $cep, $cidade, $rua, $numero, $complemento, $hora_entrada, $hora_saida, $carga_horaria, $remuneracao, $data_contratacao, $foto_perfil);
     }
 
     public function listarUsers()
@@ -26,9 +26,9 @@ class userController
         include 'C:\xampp\htdocs\gestao_bikes\GB\MVC\Views\UserViews.php';
     }
 
-    public function atualizarUser($id_user, $nome_completo, $datadenascimento, $cpf, $genero, $phone, $email, $senha, $tipo_funcionario, $cep, $cidade, $rua, $numero, $complemento, $hora_entrada, $hora_saida, $carga_horaria, $remuneracao, $data_contratacao, $foto_perfil)
+    public function atualizarUser($id_user, $nome_completo,  $nome_usuario, $datadenascimento, $cpf, $genero, $phone, $email, $senha, $tipo_funcionario, $cep, $cidade, $rua, $numero, $complemento, $hora_entrada, $hora_saida, $carga_horaria, $remuneracao, $data_contratacao, $foto_perfil)
     {
-        $this->usermodel->atualizarUser($id_user, $nome_completo, $datadenascimento, $cpf, $genero, $phone, $email, $senha, $tipo_funcionario, $cep, $cidade, $rua, $numero, $complemento, $hora_entrada, $hora_saida, $carga_horaria, $remuneracao, $data_contratacao, $foto_perfil);
+        $this->usermodel->atualizarUser($id_user, $nome_completo, $nome_usuario, $datadenascimento, $cpf, $genero, $phone, $email, $senha, $tipo_funcionario, $cep, $cidade, $rua, $numero, $complemento, $hora_entrada, $hora_saida, $carga_horaria, $remuneracao, $data_contratacao, $foto_perfil);
     }
 
     public function deletarUser($id_user)
