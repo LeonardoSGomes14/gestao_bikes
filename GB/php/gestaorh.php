b000dbe15b8e49c2df31d7e9cb5652982f6a8c71
Kauã <kaua60033@gmail.com>
2024-04-30 20:52:46 -0300

    as

--- GB/php/gestaorh.php
+++ GB/php/gestaorh.php
@@ -14,80 +14,77 @@
   <div class="texto-centralizado">
     Solicitações
   </div>
-  
 
-  <div class="search-container"> 
+
+  <div class="search-container">
     <input type="text" placeholder="Pesquisar...">
     <button type="submit">Pesquisar</button>
-    </div>
-    
-    
-    
-    
-
-    <div class="container">
-    <div class="row header">
-        <div class="column">Modelo</div>
-        <div class="column">Solicitante</div>
-        <div class="column">Responsável</div>
-        <div class="column">Situação</div>
-        <div class="column">Criado em</div>
-    </div>
-    <div class="row">
-        <div class="column">Tarefa 1</div>
-        <div class="column">João</div>
-        <div class="column">Maria</div>
-        <div class="column">Em progresso</div>
-        <div class="column">2024-04-30</div>
-    </div>
-    <div class="row">
-        <div class="column">Tarefa 2</div>
-        <div class="column">Carlos</div>
-        <div class="column">Ana</div>
-        <div class="column">Concluído</div>
-        <div class="column">2024-04-29</div>
-    </div>
-    
-</div>
-
-
-
-<h1 class="titulo"> Sistema De Gestão ERP+controle de empresas e de pessoas </h1>
-
-
-<img class="logo" src="../Img/bitrix-removebg-preview.png" width="300px">
-
-<div id="menu">
-  <div id="menu-bar" onclick="menuOnClick()">
-    <div id="bar1" class="bar"></div>
-    <div id="bar2" class="bar"></div>
-    <div id="bar3" class="bar"></div>
   </div>
-  <nav class="nav" id="nav">
-    <ul>
-      <img src="../Img/Image 2024-04-17 at 08.04.01.jpeg" style="border-radius: 50%;" width="135px" height="120px">
-      <a href="#" class="confirm-link" onclick="confirmLogout()">
 
-        <?php
 
-        echo "<h1 class='welcome-message'>Olá, " . $_SESSION["usuario"] . "</h1>";
-        ?>
-      </a>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Solicitações</a></li>
-      <li><a href="#">Recibo</a></li>
-      
+  <div class="container">
+    <div class="row header">
+      <div class="column">Modelo</div>
+      <div class="column">Solicitante</div>
+      <div class="column">Responsável</div>
+      <div class="column">Situação</div>
+      <div class="column">Criado em</div>
+    </div>
+    <div class="row">
+      <div class="column">Tarefa 1</div>
+      <div class="column">João</div>
+      <div class="column">Maria</div>
+      <div class="column">Em progresso</div>
+      <div class="column">2024-04-30</div>
+    </div>
+    <div class="row">
+      <div class="column">Tarefa 2</div>
+      <div class="column">Carlos</div>
+      <div class="column">Ana</div>
+      <div class="column">Concluído</div>
+      <div class="column">2024-04-29</div>
+    </div>
 
-    </ul>
-  </nav>
-</div>
+  </div>
 
-<div class="menu-bg" id="menu-bg"></div>
 
-<script>
-  function menuOnClick() {
-    document.getElementById("menu-bar").classList.toggle("change");
-    document.getElementById("nav").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
-  }
-</script>
+
+  <h1 class="titulo"> Sistema De Gestão ERP+controle de empresas e de pessoas </h1>
+
+
+  <img class="logo" src="../Img/bitrix-removebg-preview.png" width="300px">
+
+  <div id="menu">
+    <div id="menu-bar" onclick="menuOnClick()">
+      <div id="bar1" class="bar"></div>
+      <div id="bar2" class="bar"></div>
+      <div id="bar3" class="bar"></div>
+    </div>
+    <nav class="nav" id="nav">
+      <ul>
+        <img src="../Img/Image 2024-04-17 at 08.04.01.jpeg" style="border-radius: 50%;" width="135px" height="120px">
+        <a href="#" class="confirm-link" onclick="confirmLogout()">
+
+          <?php
+
+          echo "<h1 class='welcome-message'>Olá, " . $_SESSION["usuario"] . "</h1>";
+          ?>
+        </a>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Solicitações</a></li>
+        <li><a href="#">Recibo</a></li>
+
+
+      </ul>
+    </nav>
+  </div>
+
+  <div class="menu-bg" id="menu-bg"></div>
+
+  <script>
+    function menuOnClick() {
+      document.getElementById("menu-bar").classList.toggle("change");
+      document.getElementById("nav").classList.toggle("change");
+      document.getElementById("menu-bg").classList.toggle("change-bg");
+    }
+  </script>
