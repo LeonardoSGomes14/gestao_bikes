581f46182905dfe938a4f9436083733d708d244b
kauasilval <kaua60033@gmail.com>
2024-05-15 10:26:44 -0300

    as

---
+++ GB/php/respostasolicitacao.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Css/respostasolicitacao.css">
+    <title>GESTÃO DE BIKES</title>
+</head>
+
+<body>
+
+    <div class="comeco">
+
+        <div class="retangulo"></div>
+        <h1 class="titulo"> Sistema De Gestão ERP+controle de empresas e de pessoas </h1>
+
+        <div class="texto-centralizado">
+            <h2>Solicitações</h2>
+        </div>
+
+        <img class="logo" src="../Img/bitrix-removebg-preview.png" width="300px">
--- GB/php/solicitacaoatendidas.php
+++ GB/php/solicitacaoatendidas.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Css/solicitacao.css">
+    <link rel="stylesheet" href="../Css/solicitacaoatendidas.css">
     <title>GESTÃO DE BIKES</title>
 </head>
 
@@ -12,10 +12,7 @@
 
     <div class="comeco">
 
-        <div class="retangulo" 
-       
-
-        </div>
+        <div class="retangulo"></div>
         <h1 class="titulo"> Sistema De Gestão ERP+controle de empresas e de pessoas </h1>
 
         <div class="texto-centralizado">
@@ -24,12 +21,12 @@
 
         <img class="logo" src="../Img/bitrix-removebg-preview.png" width="300px">
 
+        <!-- Tabela de solicitações atendidas -->
         <div class="table-container">
             <table class="table">
                 <thead>
                     <tr>
                         <th>Modelo</th>
-                        <th>Solicitante</th>
                         <th>Responsável</th>
                         <th>Situação</th>
                         <th>Criado em</th>
@@ -38,21 +35,18 @@
                 <tbody>
                     <tr>
                         <td>Modelo 1</td>
-                        <td>Solicitante 1</td>
                         <td>Responsável 1</td>
                         <td>Situação 1</td>
                         <td>2024-05-13</td>
                     </tr>
                     <tr>
                         <td>Modelo 2</td>
-                        <td>Solicitante 2</td>
                         <td>Responsável 2</td>
                         <td>Situação 2</td>
                         <td>2024-05-14</td>
                     </tr>
                     <tr>
                         <td>Modelo 3</td>
-                        <td>Solicitante 3</td>
                         <td>Responsável 3</td>
                         <td>Situação 3</td>
                         <td>2024-05-15</td>
@@ -61,3 +55,6 @@
             </table>
         </div>
     </div>
+</body>
+
+</html>
