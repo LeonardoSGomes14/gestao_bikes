ef15c99bc45d233f4be4fc8d194c619a449ff18c
kauasilval <kaua60033@gmail.com>
2024-05-15 07:34:55 -0300

    as

--- GB/php/s.php
+++ GB/php/s.php
@@ -10,7 +10,7 @@ session_start(); // Certifique-se de iniciar a sessão em todas as páginas que 
 <head>
   <meta charset="UTF-8">+
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../Css/gestaorh.css">
+  <link rel="stylesheet" href="../Css/gestao_atendidas.css">
   <title>Document</title>
 </head>
 
