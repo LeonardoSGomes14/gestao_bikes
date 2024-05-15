fc784603e1690f282d99da20d4916a15874c473f
kauasilval <kaua60033@gmail.com>
2024-05-15 15:28:24 -0300

    as

--- GB/php/solicitacaoatendidas.php
+++ GB/php/solicitacaoatendidas.php
@@ -16,45 +16,11 @@
         <h1 class="titulo"> Sistema De Gestão ERP+controle de empresas e de pessoas </h1>
 
         <div class="texto-centralizado">
-            <h2>Solicitações</h2>
+            <h2>Solicitações </h2>
         </div>
 
         <img class="logo" src="../Img/bitrix-removebg-preview.png" width="300px">
 
-        <!-- Tabela de solicitações atendidas -->
-        <div class="table-container">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Modelo</th>
-                        <th>Responsável</th>
-                        <th>Situação</th>
-                        <th>Criado em</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Modelo 1</td>
-                        <td>Responsável 1</td>
-                        <td>Situação 1</td>
-                        <td>2024-05-13</td>
-                    </tr>
-                    <tr>
-                        <td>Modelo 2</td>
-                        <td>Responsável 2</td>
-                        <td>Situação 2</td>
-                        <td>2024-05-14</td>
-                    </tr>
-                    <tr>
-                        <td>Modelo 3</td>
-                        <td>Responsável 3</td>
-                        <td>Situação 3</td>
-                        <td>2024-05-15</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-</body>
+    
 
 </html>
