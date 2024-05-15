d29f4f0f473937a8e53f06ae69f63908c6459a7f
Mateus <mateusoliiveira000@gmail.com>
2024-05-15 11:17:46 -0300

    a

---
+++ GB/MVC/Model/SolicitacaoModel.php
@@ -0,0 +1,41 @@
+<?php
+class Solicitacao {
+    private $pdo;
+
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+        $this->createTable();
+    }
+
+    private function createTable() {
+        $createTableSQL = "
+            CREATE TABLE IF NOT EXISTS solicitacao (
+                id_soli INT(11) AUTO_INCREMENT PRIMARY KEY,
+                solicitante VARCHAR(255) NOT NULL,
+                responsavel VARCHAR(255) NOT NULL,
+                situacao INT(11) NOT NULL,
+                criado DATE NOT NULL
+            );
+        ";
+        $this->pdo->exec($createTableSQL);
+    }
+
+    public function getAll() {
+        $stmt = $this->pdo->query("SELECT * FROM solicitacao");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function create($solicitante, $responsavel, $situacao, $criado) {
+        $stmt = $this->pdo->prepare("
+            INSERT INTO solicitacao (solicitante, responsavel, situacao, criado) 
+            VALUES (:solicitante, :responsavel, :situacao, :criado)
+        ");
+        $stmt->execute([
+            ':solicitante' => $solicitante,
+            ':responsavel' => $responsavel,
+            ':situacao' => $situacao,
+            ':criado' => $criado,
+        ]);
+    }
+}
+?>
---
+++ GB/MVC/Views/SolicitacaoViews.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../Css/respostasolicitacao.css">
+    <title>GESTÃO DE BIKES</title>
+</head>
+
+<body>
+
+    <div class="comeco">
+
+        <div class="retangulo"></div>
+        <!DOCTYPE html>
+<html>
+<head>
+    <title>Criar Solicitação</title>
+</head>
+<body>
+    <h1>Criar Nova Solicitação</h1>
+    <form action="../../MVC/public/Solicitacao/index.php?action=create" method="POST">
+        <label for="solicitante">Solicitante:</label>
+        <input type="text" id="solicitante" name="solicitante" required><br>
+        <label for="responsavel">Responsável:</label>
+        <input type="text" id="responsavel" name="responsavel" required><br>
+        <label for="situacao">Situação:</label>
+        <input type="number" id="situacao" name="situacao" required><br>
+        <input type="submit" value="Criar">
+    </form>
+    <a href="index.php">Voltar</a>
+</body>
+</html>
+
+        <h1 class="titulo"> Sistema De Gestão ERP+controle de empresas e de pessoas </h1>
+
+        <div class="texto-centralizado">
+            <h2>Solicitações</h2>
+        </div>
+
+        <img class="logo" src="../../Img/bitrix-removebg-preview.png" width="300px">
