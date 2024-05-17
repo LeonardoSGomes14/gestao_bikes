2718ccbed7d9b6a6c75f62e83f1c3e81dd7a52a1
Mateus <mateusoliiveira000@gmail.com>
2024-05-17 13:35:20 -0300

    sa

--- GB/php/solicitacao.php
+++ GB/php/solicitacao.php
@@ -23,11 +23,11 @@
             <div class="container">
             <h2>Criar Nova Solicitação</h2>
 <form action="../MVC/public/Solicitacao/index.php?action=create" method="POST" class="form-solicitacao">
-    <label for="solicitante">Solicitante:</label>
+    <label for="solicitante">Solicitante (Não obrigatório):</label>
     <input type="text" id="solicitante" name="solicitante" required><br>
     <label for="responsavel">Responsável:</label>
     <input type="text" id="responsavel" name="responsavel" required><br>
-    <label for="situacao">Situação:</label>
+    <label for="situacao">Pedido:</label>
     <input type="text" id="situacao" name="situacao" required><br>
     <input type="submit" value="Criar">
 </form>
