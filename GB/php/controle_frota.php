7daf254b4743f2a0b8f606851b7eaa61f86cbacd
kauasilval <kaua60033@gmail.com>
2024-05-22 14:51:38 -0300

    as

--- GB/php/controle_frota.php
+++ GB/php/controle_frota.php
@@ -36,9 +36,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['edit_vehicle'])) {
     $ano_fabricado = $_POST['ano_fabricado'];
     $modelo = $_POST['modelo'];
     $tipodeveiculo = $_POST['tipodeveiculo'];
-    $imagem = addslashes(file_get_contents($_FILES['imagem']['tmp_name']));
 
-    $sql = "UPDATE controlefrota SET marca='$marca', ano_fabricado='$ano_fabricado', modelo='$modelo', tipodeveiculo='$tipodeveiculo', imagem='$imagem' WHERE id_frota=$id_frota";
+    if ($_FILES['imagem']['tmp_name']) {
+        $imagem = addslashes(file_get_contents($_FILES['imagem']['tmp_name']));
+        $sql = "UPDATE controlefrota SET marca='$marca', ano_fabricado='$ano_fabricado', modelo='$modelo', tipodeveiculo='$tipodeveiculo', imagem='$imagem' WHERE id_frota=$id_frota";
+    } else {
+        $sql = "UPDATE controlefrota SET marca='$marca', ano_fabricado='$ano_fabricado', modelo='$modelo', tipodeveiculo='$tipodeveiculo' WHERE id_frota=$id_frota";
+    }
 
     if ($conn->query($sql) === TRUE) {
         echo "Veículo atualizado com sucesso";
@@ -64,31 +68,26 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['delete_vehicle'])) {
 $vehicle_to_edit = null;
 if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['edit_vehicle'])) {
     $id_frota = $_GET['id_frota'];
-    $sql = "SELECT * FROM frota WHERE id_frota=$id_frota";
+    $sql = "SELECT * FROM controlefrota WHERE id_frota=$id_frota";
     $result = $conn->query($sql);
     $vehicle_to_edit = $result->fetch_assoc();
 }
 ?>
 
-
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../Css/controlefrota.css">
     <title>GESTÃO DE BIKES</title>
 </head>
-
 <body>
     <div class="comeco">
         <div class="retangulo"></div>
         <h1 class="titulo">Sistema De Gestão ERP+controle de empresas e de pessoas</h1>
         <img class="logo" src="../Img/bitrix-removebg-preview.png" width="300px">
     </div>
-
-  
 
     <div class="content">
         <div class="container">
@@ -104,7 +103,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['edit_vehicle'])) {
                         <input type="text" id="ano_fabricado" name="ano_fabricado" required><br>
                         <label for="modelo">Modelo:</label>
                         <input type="text" id="modelo" name="modelo" required><br>
-                        <label for="tipodeveiculo">Tipo do Produto:</label>
+                        <label for="tipodeveiculo">Tipo do Veiculo:</label>
                         <input type="text" id="tipodeveiculo" name="tipodeveiculo" required><br>
                         <label for="imagem">Imagem:</label>
                         <input type="file" id="imagem" name="imagem" required><br>
@@ -122,10 +121,10 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['edit_vehicle'])) {
                             <input type="text" id="ano_fabricado" name="ano_fabricado" value="<?php echo $vehicle_to_edit['ano_fabricado']; ?>" required><br>
                             <label for="modelo">Modelo:</label>
                             <input type="text" id="modelo" name="modelo" value="<?php echo $vehicle_to_edit['modelo']; ?>" required><br>
-                            <label for="tipodeveiculo">Tipo do Produto:</label>
+                            <label for="tipodeveiculo">Tipo do Veiculo:</label>
                             <input type="text" id="tipodeveiculo" name="tipodeveiculo" value="<?php echo $vehicle_to_edit['tipodeveiculo']; ?>" required><br>
                             <label for="imagem">Imagem:</label>
-                            <input type="file" id="imagem" name="imagem" required><br>
+                            <input type="file" id="imagem" name="imagem"><br>
                             <input type="submit" value="Atualizar Veículo">
                         </form>
                     <?php endif; ?>
@@ -149,7 +148,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['edit_vehicle'])) {
                             echo "</div>";
                             echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['imagem'] ).'"/>';
                             echo '<div class="actions">';
-                            echo '<a href="frota.php?edit_vehicle=1&id_frota='.$row["id_frota"].'">Editar</a> | <a href="frota.php?delete_vehicle=1&id_frota='.$row["id_frota"].'">Deletar</a>';
+                            echo '<a href="atualizarfrota.php"'.$row["id_frota"].'">Atualizar</a> | <a href="deletarfrota.php"'.$row["id_frota"].'">Deletar</a>';
                             echo '</div>';
                             echo "</div>";
                         }
@@ -162,8 +161,5 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['edit_vehicle'])) {
             </div>
         </div>
     </div>
-
-    
 </body>
-
 </html>
