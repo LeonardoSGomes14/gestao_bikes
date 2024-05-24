e7ac679642b37d5ecf4a34e5713279f481a0fd3d
Mateus <mateusoliiveira000@gmail.com>
2024-05-24 10:48:09 -0300

    a

--- GB/MVC/Controller/UserController.php
+++ GB/MVC/Controller/UserController.php
@@ -1,38 +1,22 @@
 <?php
 require_once 'C:\xampp\htdocs\gestao_bikes\GB\MVC\Model\UserModel.php';
 
-class usuarioController
+class UserController
 {
-    private $usermodel;
+    private $userModel;
 
     public function __construct($pdo)
     {
-        $this->usermodel = new usuarioModel($pdo);
+        
+        $this->userModel = new UserModel($pdo);
     }
 
-    public function criarUser($nome_completo, $nome_usuario, $datadenascimento, $cpf, $genero, $phone, $email, $senha, $tipo_funcionario, $cep, $cidade, $rua, $numero, $complemento, $hora_entrada, $hora_saida, $carga_horaria, $remuneracao, $data_contratacao, $foto_perfil)
+    public function displayUserList()
     {
-        $this->usermodel->criarUser($nome_completo, $nome_usuario, $datadenascimento, $cpf, $genero, $phone, $email, $senha, $tipo_funcionario, $cep, $cidade, $rua, $numero, $complemento, $hora_entrada, $hora_saida, $carga_horaria, $remuneracao, $data_contratacao, $foto_perfil);
-    }
-
-    public function listarUsers()
-    {
-        return $this->usermodel->listarUsers();
-    }
-
-    public function exibirListausers()
-    {
-        $users = $this->usermodel->listarusers();
+        $users = $this->userModel->listUsers();
         include 'C:\xampp\htdocs\gestao_bikes\GB\MVC\Views\UserViews.php';
     }
+    
 
-    public function atualizarUser($id_user, $nome_completo,  $nome_usuario, $datadenascimento, $cpf, $genero, $phone, $email, $senha, $tipo_funcionario, $cep, $cidade, $rua, $numero, $complemento, $hora_entrada, $hora_saida, $carga_horaria, $remuneracao, $data_contratacao, $foto_perfil)
-    {
-        $this->usermodel->atualizarUser($id_user, $nome_completo, $nome_usuario, $datadenascimento, $cpf, $genero, $phone, $email, $senha, $tipo_funcionario, $cep, $cidade, $rua, $numero, $complemento, $hora_entrada, $hora_saida, $carga_horaria, $remuneracao, $data_contratacao, $foto_perfil);
-    }
-
-    public function deletarUser($id_user)
-    {
-        $this->usermodel->deletarUser($id_user);
-    }
 }
+?>
--- GB/MVC/Model/UserModel.php
+++ GB/MVC/Model/UserModel.php
@@ -1,6 +1,6 @@
 <?php
-    
-class usuarioModel
+
+class UserModel
 {
     private $pdo;
 
@@ -9,60 +9,13 @@ class usuarioModel
         $this->pdo = $pdo;
     }
 
-    public function criarUser($nome_completo, $nome_usuario, $datadenascimento, $cpf, $genero, $phone, $email, $senha, $tipo_funcionario, $cep, $cidade, $rua, $numero, $complemento, $hora_entrada, $hora_saida, $carga_horaria, $remuneracao, $data_contratacao, $foto_perfil)
-    {
-        $sql = "INSERT INTO usuarios (nome_completo, nome_usuario, datadenascimento, cpf, genero, phone, email, senha, tipo_funcionario, cep, cidade, rua, numero, complemento, hora_entrada, hora_saida, carga_horaria, remuneracao, data_contratacao, foto_perfil)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([$nome_completo, $nome_usuario, $datadenascimento, $cpf, $genero, $phone, $email, $senha, $tipo_funcionario, $cep, $cidade, $rua, $numero, $complemento, $hora_entrada, $hora_saida, $carga_horaria, $remuneracao, $data_contratacao, $foto_perfil]);
-    }
-
-
-
-    public function listarUsers()
+    public function listUsers()
     {
         $sql = "SELECT * FROM usuarios";    
         $stmt = $this->pdo->query($sql);
-        return $stmt->fetchALL(PDO::FETCH_ASSOC);
-    }
-
-
-    public function
-    atualizarUser(
-    $id_user,
-    $nome_completo,
-    $nome_usuario,
-    $datadenascimento,
-    $cpf,
-    $genero,
-    $phone,
-    $email,
-    $senha,
-    $tipo_funcionario,
-    $cep,
-    $cidade,
-    $rua,
-    $numero,
-    $complemento,
-    $hora_entrada,
-    $hora_saida, 
-    $carga_horaria, 
-    $remuneracao, 
-    $data_contratacao,
-    $foto_perfil
-    ) {
-        $sql = "UPDATE usuarios SET nome_completo = ?, nome_usuario = ?, datadenascimento = ?, cpf = ?, genero = ?, phone = ?, email = ?, senha = ?, tipo_funcionario = ?, cep = ?, cidade = ?, rua = ?, numero = ?, complemento = ?, hora_entrada = ?, hora_saida = ?, carga_horaria = ?, remuneracao = ?, data_contratacao = ?, foto_perfil = ?)
-    WHERE id_user = ?";
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([$nome_completo, $nome_usuario, $datadenascimento, $cpf, $genero, $phone, $email, $senha, $tipo_funcionario, $cep, $cidade, $rua, $numero, $complemento, $hora_entrada, $hora_saida, $carga_horaria, $remuneracao, $data_contratacao, $foto_perfil, $id_user]);
-    }
-
-
- 
-    public function deletarUser($id_user)
-    {
-        $sql = "DELETE FROM usuarios WHERE id_user = ?";
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([$id_user]);
+        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+        return $users;
     }
 }
+?>
